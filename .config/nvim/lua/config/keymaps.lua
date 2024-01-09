bf40cd8b417d44f2017b7c2c0efbdf850d9ae827
saschategtmeier <accounts@saschategtmeier.dev>
2024-01-09 13:28:47 +0100

    Add new nvim lazyvim config base

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,22 @@
+-- A list of LazyVim plugins I don't want to use or haven't checked out yet.
+-- I'd rather have less than a mess of too many plugins that I don't really know how to use.
+return {
+  -- coding
+  { "echasnovski/mini.surround", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+
+  -- editor
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+  { "lewis6991/gitsigns.nvim", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
+  { "echasnovski/mini.bufremove", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+
+  -- ui
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+}
