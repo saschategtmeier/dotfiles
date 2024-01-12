e2da65cbc973d617f23c76ab19ed3e4173558ae3
saschategtmeier <accounts@saschategtmeier.dev>
2024-01-12 16:45:34 +0100

    Add don't copy x/s vim commands into register

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,7 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+-- deletes single char and doesn't copy into register
+keymap.set("n", "x", '"_x')
+keymap.set("n", "s", '"_s')
