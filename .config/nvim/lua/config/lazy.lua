fa7b4350fec556b3515bc85b1950d4362286f3d4
saschategtmeier <accounts@saschategtmeier.dev>
2024-01-13 21:42:08 +0100

    Add markdown lang extra and update packages

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
 
     -- extras.lang
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
